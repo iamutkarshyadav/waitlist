ac4d910f170127086ac6d9ff2132d2819253de14
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2026-01-12 22:03:11 +0530

    final touches

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+  devIndicators: {
+    buildActivity: false,
+  },
+  experimental: {
+    devToolbar: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
